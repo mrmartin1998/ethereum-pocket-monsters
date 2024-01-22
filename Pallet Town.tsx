77943754392d7f19f8dcd5019f5fb1aaea6f5a07
Martin Emil Brabenec <martinemilbrabenec@gmail.com>
2024-01-22 16:20:38 +0100

    new assests

---
+++ Pallet Town.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Pallet Town" tilewidth="32" tileheight="32" tilecount="132" columns="12">
+ <image source="Assets/Pallet Town.png" width="384" height="365"/>
+</tileset>
